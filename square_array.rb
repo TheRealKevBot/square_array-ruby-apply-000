ff817b7ea360ff04abfad23227d9fdd1c356caf9
Kevin Glasgow <kglas001@gmail.com>
2020-08-22 00:18:56 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,6 @@
 def square_array(numbers)
   numbers = [1,2,3]
-  square_numbers = []
-  numbers.each do |number|
-    square_numbers << (number ** 2)
+  numbers
   end
   return square_numbers
 end 
