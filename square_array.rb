6adaaafeeb3d12ca47cc66105341a77e1ff6aa6c
Kevin Glasgow <kglas001@gmail.com>
2020-08-22 00:24:50 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,9 @@
-#def square_array(numbers)
-  #numbers = [1,2,3]
-#  sq_numbers = [] 
-#  numbers.each do |x|
-#    sq_numbers << (x ** 2) 
-#  end
-#  return sq_numbers
-#end 
-
 def square_array(numbers)
-   sq_numbers = numbers.collect{|x| x ** 2}
-   return sq_numbers
- end
+  #numbers = [1,2,3]
+  sq_numbers = [] 
+  numbers.each do |x|
+    sq_numbers << (x ** 2) 
+  end
+  return sq_numbers
+end 
+
